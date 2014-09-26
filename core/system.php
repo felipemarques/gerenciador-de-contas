3399f46eecc30b7a6186eb64ccc645bba4fcb542
Renato Rodrigues <renato.leandrorodrigues@gmail.com>
2014-09-25 23:44:19 -0300

    ++ config, Arquivo responsável pelo "include" das classes e arquivos

---
+++ core/system.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ *
+ * @author Renato Rodrigues renato@rlsrodrigues.com.br
+ * Includes necessary files to suystem
+ *
+ **/
+set_include_path('core/');
+require_once('config/config.php');
+require_once('db/Db.php');
+
+
+?>
