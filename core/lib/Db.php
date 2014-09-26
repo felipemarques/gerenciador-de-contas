92951158ffce8c2f82100f476c428b18ada91c85
Renato Rodrigues <renato.leandrorodrigues@gmail.com>
2014-09-26 04:19:55 -0300

    ++ log, distribuindo e organizando logs

--- core/lib/Db.php
+++ core/lib/Db.php
@@ -19,8 +19,14 @@ class Db
  	 */
  	public function __construct($table){
 
- 		//seting the table
- 		$this->setTable($table);
+ 		try {
+
+	 		//seting the table
+	 		$this->setTable($table);
+ 			
+ 		} catch (Exception $e) {
+ 			Log::createLog('Não foi possível setar a tabela, erro: ' . $e);
+ 		}
 
  	}
 
@@ -49,23 +55,30 @@ class Db
  	public function getResultsObject($fields="", $where = "", $limit = "")
  	{
 
+ 		try {
 
- 		if($fields != "" && is_array($fields)):
- 			$sql = 'SELECT ' . implode(',', $fields) . ' FROM ' . DB::$table;
- 		else:
- 			$sql = 'SELECT * FROM ' . DB::$table;
- 		endif;
+ 			if($fields != "" && is_array($fields)):
+	 			$sql = 'SELECT ' . implode(',', $fields) . ' FROM ' . DB::$table;
+	 		else:
+	 			$sql = 'SELECT * FROM ' . DB::$table;
+	 		endif;
 
- 		if($where != "") 			
- 			$sql .=' WHERE ' . $where . '';
+	 		if($where != "") 			
+	 			$sql .=' WHERE ' . $where . '';
 
- 		if($limit != "") 						
- 			$sql .=' LIMIT '.$limit.'';
+	 		if($limit != "") 						
+	 			$sql .=' LIMIT '.$limit.'';
 
- 		$result = Connect::getInstance()->query($sql);
-		$result = $result->fetchAll(PDO::FETCH_OBJ);
+	 		$result = Connect::getInstance()->query($sql);
+			$result = $result->fetchAll(PDO::FETCH_OBJ);
 
-		return $result;
+			return $result;
+ 			
+ 		} catch (Exception $e) {
+
+ 			Log::createLog('Não foi possível obter os resultados do objeto, erro: ' . $e);
+ 			
+ 		}
 
  	}
 
@@ -80,19 +93,25 @@ class Db
  	 **/
  	public function getResultRow($fields = "", $where = "")
  	{
- 		if($fields != "" && is_array($fields)):
- 			$sql = 'SELECT ' . implode(',', $fields) . ' FROM ' . DB::$table;
- 		else:
- 			$sql = 'SELECT * FROM ' . DB::$table;
- 		endif;
+ 		try {
 
- 		if($where != "") 			
- 			$sql .=' WHERE ' . $where . '';
+ 			if($fields != "" && is_array($fields)):
+	 			$sql = 'SELECT ' . implode(',', $fields) . ' FROM ' . DB::$table;
+	 		else:
+	 			$sql = 'SELECT * FROM ' . DB::$table;
+	 		endif;
 
- 		$result = Connect::getInstance()->query($sql);
-		$result = $result->fetch(PDO::FETCH_OBJ);
+	 		if($where != "") 			
+	 			$sql .=' WHERE ' . $where . '';
 
-		return $result;
+	 		$result = Connect::getInstance()->query($sql);
+			$result = $result->fetch(PDO::FETCH_OBJ);
+
+			return $result;
+ 			
+ 		} catch (Exception $e) {
+ 			Log::createLog('Não foi possível obter os resultados da linha, erro: ' . $e);
+ 		}
  	}
 
  	/**
@@ -108,31 +127,39 @@ class Db
  	public function updateData($fields, $values, $column, $id)
  	{
 
- 		if(is_array($fields) && is_array($values) && isset($id) && isset($column)){
+ 		try {
+ 			
+ 			if(is_array($fields) && is_array($values) && isset($id) && isset($column)){
 
- 			if(count($fields) == count($values)){
- 				
- 				$content = '';
- 				$i = 0;
+	 			if(count($fields) == count($values)){
+	 				
+	 				$content = '';
+	 				$i = 0;
 
- 				while($i<count($fields)){
- 					$content .= $fields[$i] . '="'.$values[$i].'"';
- 					$content .= $i < count($fields)-1 ? ', ' : '';
- 					$i++;
- 				}
- 				
- 				$sql = 'UPDATE ' . DB::$table . ' SET ' . $content .' WHERE ' . $column . ' = ' . $id;
+	 				while($i<count($fields)){
+	 					$content .= $fields[$i] . '="'.$values[$i].'"';
+	 					$content .= $i < count($fields)-1 ? ', ' : '';
+	 					$i++;
+	 				}
+	 				
+	 				$sql = 'UPDATE ' . DB::$table . ' SET ' . $content .' WHERE ' . $column . ' = ' . $id;
 
- 				$p_sql = Connect::getInstance()->prepare($sql);
- 				return $p_sql->execute();
+	 				$p_sql = Connect::getInstance()->prepare($sql);
+	 				return $p_sql->execute();
 
- 			}else{
- 				//colocar erro de diferença de valores nos arrays
- 			}
+	 			}else{
+	 				Log::createLog('Nós não conseguimos fazer o update quando você envia arrays de tamanho diferente!');
+	 			}
 
 
- 		}else{
- 			//colocar erro de parametros vazios
+	 		}else{
+	 			Log::createLog('Por gentileza envie os dados necessários para o update');
+	 		}
+
+ 		} catch (Exception $e) {
+
+ 			Log::createLog('Encontramos um problema e não conseguimos fazer o update, erro: ' . $e);
+ 			
  		}
 
  	}
@@ -147,23 +174,28 @@ class Db
  	 **/
  	public function insertData($fields, $values)
  	{
+ 		try {
 
- 		if(is_array($fields) && is_array($values)){
+ 			if(is_array($fields) && is_array($values)){
 
- 			if(count($fields) == count($values)){
- 				
- 				$sql = 'INSERT INTO ' . DB::$table . ' (' . implode(',', $fields) . ') VALUES ("' . implode('","', $values) . '")';
+	 			if(count($fields) == count($values)){
+	 				
+	 				$sql = 'INSERT INTO ' . DB::$table . ' (' . implode(',', $fields) . ') VALUES ("' . implode('","', $values) . '")';
 
- 				$p_sql = Connect::getInstance()->prepare($sql);
- 				return $p_sql->execute();
+	 				$p_sql = Connect::getInstance()->prepare($sql);
+	 				return $p_sql->execute();
 
- 			}else{
- 				//echo 'seu burro você está mandando um array com mais itens que o outro';
- 			}
+	 			}else{
+	 				Log::createLog('Nós não conseguimos fazer o insert quando você envia arrays de tamanho diferente!');
+	 			}
 
 
- 		}else{
- 			//echo 'cara, você precisa setar os campos necessários na chamada da função';
+	 		}else{
+	 			Log::createLog('Por gentileza envie os dados necessários para o update');
+	 		}
+ 			
+ 		} catch (Exception $e) {
+ 			Log::createLog('Encontramos um problema e não conseguimos fazer o insert, erro: ' . $e);
  		}
 
  	}
@@ -178,18 +210,27 @@ class Db
  	 **/
  	public function deleteData($column, $id)
  	{
+ 		try {
 
- 		if(isset($column) && is_array($id)){
+ 			if(isset($column) && is_array($id)){
 
- 			$sql = 'DELETE FROM ' . DB::$table . ' WHERE  ' . $column . ' in ( ' . implode(", ", $id) . ')';
+	 			$sql = 'DELETE FROM ' . DB::$table . ' WHERE  ' . $column . ' in ( ' . implode(", ", $id) . ')';
 
- 		}else{
- 			echo 'cara, você precisa setar os campos necessários na chamada da função';
- 			return false;
+	 		}else{
+
+	 			Log::createLog('Por gentileza envie os dados necessários para o delete');
+	 			return false;
+	 			
+	 		}
+
+	 		$p_sql = Connect::getInstance()->prepare($sql);
+	 		return $p_sql->execute();
+ 			
+ 		} catch (Exception $e) {
+
+ 			Log::createLog('Encontramos um problema e não conseguimos fazer o delete, erro: ' . $e);
+ 			
  		}
-
- 		$p_sql = Connect::getInstance()->prepare($sql);
- 		return $p_sql->execute();
 
  	}
 
